4d1ce1fa1d1f2184fc5458d3b90a86e433d79e12
Arash <a.donyagard@yahoo.com>
2020-06-07 19:44:31 +0430

    implement detail fragment

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt
@@ -20,10 +20,11 @@ package com.example.android.marsrealestate.overview
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import com.example.android.marsrealestate.R
 import com.example.android.marsrealestate.databinding.FragmentOverviewBinding
-import com.example.android.marsrealestate.databinding.GridViewItemBinding
 import com.example.android.marsrealestate.network.MarsApiFilter
 
 /**
@@ -53,7 +54,16 @@ class OverviewFragment : Fragment() {
         binding.viewModel = viewModel
 
         setHasOptionsMenu(true)
-        binding.photosGrid.adapter = PhotoGridAdapter()
+        binding.photosGrid.adapter = PhotoGridAdapter(PhotoGridAdapter.OnClickListener {
+            viewModel.displayPropertyDetails(it)
+        })
+        viewModel.navigateToSelectedProperty.observe(viewLifecycleOwner, Observer {
+            if (null != it) {
+                this.findNavController().navigate(
+                        OverviewFragmentDirections.actionShowDetail(it))
+                viewModel.displayPropertyDetailsComplete()
+            }
+        })
         return binding.root
     }
 
@@ -67,7 +77,7 @@ class OverviewFragment : Fragment() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         viewModel.updateFilter(
-                when(item.itemId) {
+                when (item.itemId) {
                     R.id.show_rent_menu -> MarsApiFilter.SHOW_RENT
                     R.id.show_buy_menu -> MarsApiFilter.SHOW_BUY
                     else -> MarsApiFilter.SHOW_ALL
